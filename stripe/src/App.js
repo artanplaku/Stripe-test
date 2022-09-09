318b0ea21037342114d000320cfdfbf7020dca44
Artan <artanpl03@gmail.com>
2022-09-08 23:48:22 -0400

    changed app and create components

--- stripe/src/App.js
+++ stripe/src/App.js
@@ -1,53 +1,23 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+import StripeContainer from './components/StripeContainer';
 
-const ProductDisplay = () => (
-  <section>
-    <div className="product">
-      <img
-        src="https://i.imgur.com/EHyR2nP.png"
-        alt="The cover of Stubborn Attachments"
-      />
-      <div className="description">
-      <h3>Stubborn Attachments</h3>
-      <h5>$20.00</h5>
-      </div>
-    </div>
-    <form action="/create-checkout-session" method="POST">
-      <button type="submit">
-        Checkout
-      </button>
-    </form>
-  </section>
-);
-
-const Message = ({ message }) => (
-  <section>
-    <p>{message}</p>
-  </section>
-);
-
-export default function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-
-    if (query.get("success")) {
-      setMessage("Order placed! You will receive an email confirmation.");
-    }
-
-    if (query.get("canceled")) {
-      setMessage(
-        "Order canceled -- continue to shop around and checkout when you're ready."
-      );
-    }
-  }, []);
-
-  return message ? (
-    <Message message={message} />
-  ) : (
-    <ProductDisplay />
-  );
+function App() {
+	const [showItem, setShowItem] = useState(false);
+	return (
+		<div className='App'>
+			<h1>The Spatula Store</h1>
+			{showItem ? (
+				<StripeContainer />
+			) : (
+				<>
+					<h3>$10.00</h3>
+					<img src={spatula} alt='Spatula' />
+					<button onClick={() => setShowItem(true)}>Purchase Spatula</button>
+				</>
+			)}
+		</div>
+	);
 }
+
+export default App;
---
+++ stripe/src/components/StripeContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const PUBLIC_KEY = "pk_test_51LMDe1FaVR1BroVgrMmVLG472IjaIryywSqUjIugKZhMGo4T0Ciqw1DJX1QdUh63u76CTzOM4YACzU6mODSo373j009Xs1VhY2"
+
+const StripePromise = loadStripe(PUBLIC_KEY)
+
+const StripeContainer = () =>{
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default StripeContainer
