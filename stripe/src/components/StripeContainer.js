649d73719b5147ed4038878b47dd619f6f0d1397
Artan <artanpl03@gmail.com>
2022-09-14 23:55:37 -0400

    added backend and changed Stripe container

--- stripe/src/components/StripeContainer.js
+++ stripe/src/components/StripeContainer.js
@@ -5,13 +5,13 @@ import PaymentForm from "./PaymentForm";
 
 const PUBLIC_KEY = "pk_test_51LMDe1FaVR1BroVgrMmVLG472IjaIryywSqUjIugKZhMGo4T0Ciqw1DJX1QdUh63u76CTzOM4YACzU6mODSo373j009Xs1VhY2"
 
-const StripePromise = loadStripe(PUBLIC_KEY)
+const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
 const StripeContainer = () =>{
 
 
     return(
-        <Elements>
+        <Elements stripe={stripeTestPromise}>
             <PaymentForm />
         </Elements>
     )
